22daa25c43ca6a7b8ff1549522bf40b7aac1bcc8
robreijtenbach <33518020+robreijtenbach@users.noreply.github.com>
2025-01-10 17:48:57 +0100

    Added basic logger

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 from anagram import Anagram
 from station_scraper import StationScraper
 
+from datetime import datetime
+
 scraper = StationScraper()
 
 anagram = Anagram(scraper.getNames())
@@ -11,8 +13,13 @@ anagram = Anagram(scraper.getNames())
 TOKEN = open("token.txt", "r").readline()
 BOT_Username = "TODO"
 
+def log(txt):
+    with open("log.txt", 'a') as f:
+        f.write(f"{datetime.now()}: {txt}\n")
+
 # Commands
 async def start_command(update, context):
+    log(f"Update: {update}")
     await update.message.reply_text("Hallo, stuur een berichtje met de"\
                                     "gehusselde letters van een stationsnaam"\
                                     "zoals in de NS puzzels en je krijgt het"\
@@ -20,6 +27,7 @@ async def start_command(update, context):
 
 # Messages
 async def handle_message(update, context):
+    log(f"Update: {update}")
     text = anagram.getAnagram(update.message.text)
     if not text:
         text = "Voor deze input heb ik geen station kunnen vinden, weet je"\
@@ -29,10 +37,11 @@ async def handle_message(update, context):
 
 # Errors
 async def handle_error(update, context):
+    log(f"Update: {update}")
     print(f"Update: {update}, caused error: {context.error}")
 
 if __name__ == "__main__":
-    app =Application.builder().token(TOKEN).build()
+    app = Application.builder().token(TOKEN).build()
 
     # Command Handlers
     app.add_handler(CommandHandler("start", start_command))
